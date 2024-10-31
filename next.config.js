ed38f34690fe4c5760ab07fe3b093b25a77b193a
mxplayermovies <127490398+mxplayermovies@users.noreply.github.com>
2024-11-01 05:03:31 +0530

    ok update

--- next.config.js
+++ next.config.js
@@ -1,14 +1,34 @@
-// next.config.js
 
+
+// // next.config.js
 // module.exports = {
-//     images: {
-//       domains: ['uwatchfreeonline.vercel.app'],
-//     },
-//   };
-  
-
-
-
+//   /* Custom webpack configuration */
+//   webpack: (config, { dev, isServer }) => {
+//     // Modify webpack configuration here
+//     return config;
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'yt3.googleusercontent.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: '123moviesonline.vercel.app',
+//       },
+//       {
+//         protocol: 'http',
+//         hostname: 'localhost',
+//         port: '3000', // Assuming you're serving from port 3000 locally
+//       },
+//     ],
+//   },
+// };
 
 // next.config.js
 module.exports = {
@@ -38,5 +58,8 @@ module.exports = {
       },
     ],
   },
+  // Enabling future/experimental features for Next.js
+  experimental: {
+    appDir: true, // Enable app directory for Next.js 13+
+  },
 };
-
